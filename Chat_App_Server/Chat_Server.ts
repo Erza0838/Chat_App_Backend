a54429cdbcad58cca1aaa71fee059cfbdaeabbeb
Erza0838 <muhammaderza884@gmail.com>
2024-06-18 00:05:16 +0700

    Perbaikan-4 API register

--- Chat_App_Server/Chat_Server.ts
+++ Chat_App_Server/Chat_Server.ts
@@ -1,9 +1,10 @@
-import express, { Application,Request, Response, Express, response} from "express"
+import express, { Application,Request, Response, Express, response, query, NextFunction} from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import helmet from "helmet"
 import axios from "axios"
-// import domparser from "domparser"
+import { AxiosError } from "axios"
+// import * as fetch from "node-fetch"
 
 const app:Application = express()
 const PORT = 3000
@@ -20,12 +21,11 @@ import { compareSync } from "bcrypt"
 import { error } from "winston"
 import { any } from "zod"
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
-
 const FrontendApiUrlReference: FrontendApiUrl =
+
 {
     RegisterUrl: "http://localhost:5173/LoginUser/RegisterUser/"
 }
@@ -46,70 +46,39 @@ function ConnectionPort(): void
 }
 ConnectionPort()
 
-function ShowLoginInformation(): void
-{   
-    app.post("/LoginPage", (request: Request, response: Response) => 
-    {   
-        // Type untuk menyimpan informasi login user
-        let UserLoginInformation : { UserEmail: string, UserPassword: string} = 
-        {
-            UserEmail: request.body.LoginEmail,
-            UserPassword: request.body.LoginPassword
-        }
-    })  
-}
-
 // Perbaikan register api
 app.post("/api/User/Register", async (request: Request, response: Response) => 
 {   
-    // const ResponseData = await axios.post<CreateNewAccountRequest[]>(FrontendApiUrlReference.RegisterUrl)
-    // const result = ResponseData.data
-    // response.json(result.map((InterfaceProperty) => 
-    // ({
-    //     InsertGender: InterfaceProperty.InsertGenders,
-    //     CreateEmail: InterfaceProperty.CreateEmail,
-    //     CreatePassword: InterfaceProperty.CreatePassword,  
-    //     CreateUserName: InterfaceProperty.CreateUserName
-    // })))
-    const ResponseData = await fetch(FrontendApiUrlReference.RegisterUrl)
-    const data = await ResponseData.json() 
-    console.log(data)
-    return data
+    await fetch("http://localhost:5173/LoginUser/RegisterUser/")
+    .then(Response => Response.json()) 
+    .then(data => data)
+    .catch(error => console.log(error))
+
+    // try
+    // {   
+    //     await fetch("http://localhost:5173/LoginUser/RegisterUser/")
+    //     .then(Response => Response.json()) 
+    //     .then(data => data)
+    //     .catch(error => console.log(error))
+    // }
+    // catch(error)
+    // {
+    //     console.log(error)
+    // }  
+    
+    // try 
+    // {
+    //     axios.post("http://localhost:5173/LoginUser/RegisterUser/")
+    //     .then(response => console.log(response))
+    //     .then(data => console.log(data))
+    //     .catch(error => console.log(error))
+    // } 
+    // catch (error) 
+    // {   
+    //     if(axios.isAxiosError(error))
+    //     {
+    //         console.log("error message:",error.message)
+    //     }
+    // }
 })
-
-// Function untuk memasukkan data data user membuat akun baru
-function NewRegisterUser(): void
-{           
-    app.post("/api/User/Register", async (request: Request, response: Response) => 
-    {       
-        // FetchData()
-        // const RegiserData : CreateNewAccountRequest = request.body as CreateNewAccountRequest
-        // try 
-        // {
-        //     const responseData = await fetch(FrontendApiUrlReference.RegisterUrl, 
-        //     {
-        //         method: "POST",
-        //         headers: 
-        //         {
-        //         "Content-Type":"application/json"
-        //         },
-        //         body: JSON.stringify({
-        //         Gender: RegiserData.InsertGenders,
-        //         Email: RegiserData.CreateEmail,
-        //         Username: RegiserData.CreateUserName,
-        //         Password: RegiserData.CreatePassword        
-        //         })
-        //     })
-        //       .then(response => console.log(response))
-        //       .then(data => console.log(data))
-        //       .catch(error => console.log(error))
-        // } 
-        // catch (error) 
-        // {
-        //     console.log(error)
-        // }
-    })
-}
-// NewRegisterUser()
-
 export default app
